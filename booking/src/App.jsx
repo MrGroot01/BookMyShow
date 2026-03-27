f20700fd76b0a2c2f3729e220ed44699e05e0f5c
Teju2002-alt <tejupateltejupatel0@gmail.com>
2026-03-27 12:27:05 +0530

    Update greeting message in App component to include 'teju'

--- booking/src/App.jsx
+++ booking/src/App.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <h1>hello Jaswanths</h1>
-=======
-      <h1>hello Jaswanth kiran teju</h1>
->>>>>>> 4bb91f34418d0bdd4613245c56a476cbcb01757d
+      <h1>hello Jaswanths teju</h1>
     </div>
   )
 }
