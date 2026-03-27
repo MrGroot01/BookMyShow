3681b2e2f2d4ba2487aeaaa26e51d230b5da6f8d
Kiran <kirand09876@gmail.com>
2026-03-27 11:35:39 +0530

    Initial commit

--- booking/src/Components/Components.jsx
+++ booking/src/Components/Components.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const Components = () => {
   return (
     <div>
-      <h1>hello</h1>
+      
     </div>
   )
 }
 
-export default App
+export default Components
