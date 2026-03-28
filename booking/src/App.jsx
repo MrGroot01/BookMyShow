05bc37fb7d5f938565b3148ffc3957c1692c79b4
ShivaShankar R <shivashankarr9035@gmail.com>
2026-03-28 12:00:03 +0530

    Movies Section

--- booking/src/App.jsx
+++ booking/src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-<<<<<<< HEAD
-import Homepage from './Components/HomePage/Homepage'
-=======
 
->>>>>>> ae833f2db0b06505445ad02ec7f4a2401192a9e1
+import Homepage from './Components/HomePage/Homepage'
 import Navbar from './NavBar/Navbar'
 import Movies from './Components/Movies/Movies'
 import Login from './NavBar/Login/Login'
@@ -18,10 +15,6 @@ const App = () => {
         <Route path='/' element={<Homepage />} />
         <Route path='movies' element={<Movies />} />
         <Route path='login' element={<Login />} />
-<<<<<<< HEAD
-
-=======
->>>>>>> ae833f2db0b06505445ad02ec7f4a2401192a9e1
       </Routes>
 
     </div>
