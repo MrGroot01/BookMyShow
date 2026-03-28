93b478825d450b6a4d0916589f7e3b2692123f05
Kiran <kirand09876@gmail.com>
2026-03-28 10:39:42 +0530

    intialize commit

--- booking/src/App.jsx
+++ booking/src/App.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-<<<<<<< HEAD
-import Homepage from './Components/HomePage/Homepage'
-const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Homepage/>} />
-=======
+
 import Navbar from './NavBar/Navbar'
 import Homepage from './Components/HomePage/Homepage'
 import Movies from './Components/Movies/Movies'
@@ -17,12 +10,13 @@ const App = () => {
   return (
     <div>
       <Navbar />
+
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='movies' element={<Movies />} />
         <Route path='login' element={<Login />} />
->>>>>>> 5350464072e06e8ebe25a0b0bf55a91647e04bee
       </Routes>
+
     </div>
   )
 }
