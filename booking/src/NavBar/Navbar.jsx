eb4d0f699883839c381df85866eff5db5ea0972c
Teju2002-alt <tejupateltejupatel0@gmail.com>
2026-03-29 09:58:24 +0530

    refactor(Navbar.jsx): import useState for state management in the Navbar component

--- booking/src/NavBar/Navbar.jsx
+++ booking/src/NavBar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaFilm,
   FaMusic,
@@ -7,7 +7,7 @@ import {
   FaHiking,
   FaChild,
 }from "react-icons/fa";
-import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
