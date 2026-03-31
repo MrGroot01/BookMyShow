7a7dbefd9aaf20815a2d619482c2a2a0ef5dda19
Teju2002-alt <tejupateltejupatel0@gmail.com>
2026-03-31 12:48:15 +0530

    my changes

--- booking/src/App.jsx
+++ booking/src/App.jsx
@@ -10,29 +10,16 @@ import EventDetails from "./Components/ComedyShow/EventDetails";
 import Sports from "./Components/Sports/Sports";
 import Adventure from "./Components/Adventure/Adventure";
 import Kids from "./Components/Kids/Kids";
-import Login from "./NavBar/Login/Login"
 
+// ✅ Only ONE import for login modal
 import LoginModal from "./NavBar/Login/Login";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='movies' element={<Movies />} />
-        <Route path='login' element={<Login />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/concerts' element={<Concerts/>} />
-        <Route path='/comedyshow' element={<ComedyShow/>} />
-        <Route path='/sports' element={<Sports/>} />
-        <Route path='/adventure' element={<Adventure/>} />
-        <Route path='/kids' element={<Kids/>} />
-      </Routes>
-      
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-
 
   return null;
 }
@@ -51,12 +38,13 @@ const App = () => {
         <Route path="/movies" element={<Movies />} />
         <Route path="/concerts" element={<Concerts />} />
         <Route path="/comedyshow" element={<ComedyShow />} />
-        <Route path="/event/:id" element={<EventDetails />} />
+        <Route path="/event/:id" element={<EventDetails />} /> {/* ✅ important */}
         <Route path="/sports" element={<Sports />} />
         <Route path="/adventure" element={<Adventure />} />
         <Route path="/kids" element={<Kids />} />
       </Routes>
 
+      {/* ✅ Login Popup */}
       {openLogin && <LoginModal close={() => setOpenLogin(false)} />}
     </>
   );
