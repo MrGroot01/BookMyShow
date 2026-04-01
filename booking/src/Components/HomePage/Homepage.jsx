a245324f1e150568032ddc9ed6219e59a9a88328
Kiran <kirand09876@gmail.com>
2026-04-01 13:09:41 +0530

    homecss

--- booking/src/Components/HomePage/Homepage.jsx
+++ booking/src/Components/HomePage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import "./HomePage.css";
+import "./Homepage.css";
 import { useNavigate } from "react-router-dom";
 
 const heroSlides = [
